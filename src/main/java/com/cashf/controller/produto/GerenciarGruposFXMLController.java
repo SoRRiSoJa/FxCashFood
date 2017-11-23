038a1ede463420d80b4eb9b040ba88e9ff0fcea7
joao <joao@joaoDesk>
2017-11-23 01:07:59 -0200

    Implementados Controller Grupos

--- src/main/java/com/cashf/controller/produto/GerenciarGruposFXMLController.java
+++ src/main/java/com/cashf/controller/produto/GerenciarGruposFXMLController.java
@@ -17,6 +17,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 /**
@@ -26,7 +27,16 @@ import javafx.scene.input.MouseEvent;
  */
 public class GerenciarGruposFXMLController implements Initializable {
 
-        
+    //----------------------------------------------
+    private GrupoController controller = new GrupoController();
+    private String erros;
+    private boolean flagButtons;
+    private String descricao;
+    private String descricaoCategoria;
+    private long idGrupo = 0;
+    private long idCat = 0;
+    @FXML
+    private TableColumn<Categoria, String> tbcDescricaoCat;
     @FXML
     private JFXButton btnSalvar;
     @FXML
@@ -46,15 +56,15 @@ public class GerenciarGruposFXMLController implements Initializable {
     @FXML
     private TableView<Categoria> tbvCategoria;
     @FXML
-    private TableColumn<Categoria, Integer> tbcCod;
-    @FXML
-    private TableColumn<Categoria, String> tbcDescricao;
+    private TableColumn<Categoria, Integer> tbcCodCat;
     @FXML
     private TableColumn btnDeletar;
     @FXML
     private TableView<Grupo> tbvGrupos;
     @FXML
-    private TableColumn<Grupo, String> tbcCategoria;
+    private TableColumn<Grupo, String> tbcDescricaoGrupo;
+    @FXML
+    private TableColumn<Grupo, String> tbcCategoriaGrupo;
 
     /**
      * Initializes the controller class.
@@ -62,30 +72,76 @@ public class GerenciarGruposFXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-
+        loadCbbCategoria();
+        setInputOff();
+    }
+    
     @FXML
     private void onSalvar(ActionEvent event) {
     }
-
+    
     @FXML
     private void onNovo(ActionEvent event) {
+        setInputON();
+        clearFields();
+        btnNovo.setDisable(true);
+        btnExcluir.setDisable(true);
     }
-
+    
     @FXML
     private void onExcluir(ActionEvent event) {
     }
-
+    
     @FXML
     private void onLimpar(ActionEvent event) {
+        clearFields();
     }
-
+    
     @FXML
     private void onAdicionar(ActionEvent event) {
     }
-
+    
     @FXML
     private void onMouseClicked(MouseEvent event) {
     }
     
+    private void clearFields() {
+        txtDescricao.clear();
+        txtDescCat.clear();
+        cbbCategoria.setValue(null);
+    }
+    
+    private void setInputON() {
+        btnExcluir.setDisable(false);
+        btnLimpar.setDisable(false);
+        btnSalvar.setDisable(false);
+        txtDescricao.setDisable(false);
+        txtDescCat.setDisable(false);
+        cbbCategoria.setDisable(false);
+        flagButtons = true;
+    }
+    
+    private void setInputOff() {
+        btnExcluir.setDisable(true);
+        btnLimpar.setDisable(true);
+        btnSalvar.setDisable(true);
+        txtDescCat.setDisable(true);
+        txtDescricao.setDisable(true);
+        cbbCategoria.setDisable(true);
+        flagButtons = false;
+    }
+    
+    private void setUptableView() {
+        tbcDescricaoGrupo.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+        tbcCategoriaGrupo.setCellValueFactory(new PropertyValueFactory<>("categoria"));
+        tbvGrupos.getColumns().setAll(tbcDescricaoGrupo, tbcCategoriaGrupo);
+    }
+
+    private void loadtbv() {
+        tbvGrupos.setItems(controller.getLista());
+    }
+    
+    private void loadCbbCategoria() {
+        cbbCategoria.setItems(controller.getListaCategria());
+    }
 }
---
+++ src/main/java/com/cashf/controller/produto/GrupoController.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cashf.controller.produto;
+
+import com.cashf.dao.produto.CategoriaDAO;
+import com.cashf.dao.produto.GrupoDAO;
+import com.cashf.model.produto.Categoria;
+import com.cashf.model.produto.Grupo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author joao
+ */
+public class GrupoController {
+
+    private final GrupoDAO grupoDAO;
+    private final CategoriaDAO categoriaDAO;
+    private ObservableList<Grupo> lista;
+    private ObservableList<Categoria> listaCategria;
+    private Grupo grupo;
+    private Categoria categoria;
+
+    public GrupoController() {
+        this.grupoDAO = new GrupoDAO(Grupo.class);
+        this.categoriaDAO = new CategoriaDAO(Categoria.class);
+        this.lista = FXCollections.observableList(grupoDAO.listAll());
+        this.listaCategria = FXCollections.observableList(categoriaDAO.listAll());
+        this.grupo = new Grupo();
+        this.categoria = new Categoria();
+        this.grupo.setIdGrupo(0);
+        this.categoria.setIdCategoria(0);
+    }
+
+    public ObservableList<Grupo> getLista() {
+        return lista;
+    }
+
+    public void setLista(ObservableList<Grupo> lista) {
+        this.lista = lista;
+    }
+
+    public void setItemLista(Grupo item) {
+        this.lista.add(item);
+    }
+
+    public ObservableList<Categoria> getListaCategria() {
+        return listaCategria;
+    }
+
+    public void setListaCategria(ObservableList<Categoria> listaCategria) {
+        this.listaCategria = listaCategria;
+    }
+
+    public void setItemListaCategria(Categoria categria) {
+        this.listaCategria.add(categria);
+    }
+
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
+    }
+
+    public void setGrupo(long id, String descricao) {
+        this.grupo = new Grupo(id, descricao, categoria);
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setCategoria(long id, String descricao) {
+        this.categoria = new Categoria(id, descricao);
+    }
+
+    public void insert() {
+        grupoDAO.save(grupo);
+        setItemLista(grupo);
+    }
+
+    public void delete() {
+        grupoDAO.delete(grupo);
+        lista.remove(grupo);
+    }
+
+    public void update() {
+        grupoDAO.update(grupo);
+    }
+
+}
