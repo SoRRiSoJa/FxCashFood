9a029b71bafbf6f6f9e9b1e40ff1dff111fcb293
joao <joao@joaoDesk>
2017-12-13 19:31:42 -0200

    Criado e implementado Parametros Controller

---
+++ src/main/java/com/cashf/controller/parametrizacao/ParametrosController.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cashf.controller.parametrizacao;
+
+import com.cashf.dao.parametros.ParametrosDAO;
+import com.cashf.dao.usuario.UsuarioDAO;
+import com.cashf.model.cidade.Cidade;
+import com.cashf.model.parametros.Parametros;
+import com.cashf.model.usuario.UNivel;
+import com.cashf.model.usuario.Usuario;
+import controller.GenericController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author joao
+ */
+public class ParametrosController implements GenericController<Parametros> {
+
+    public static ParametrosController parametrosController = null;
+    private final ParametrosDAO parametrosDAO;
+    private final UsuarioDAO usuarioDAO;
+    private ObservableList<Parametros> lista;
+    private Parametros parametro;
+    private Usuario usuario;
+    
+
+    private ParametrosController() {
+        this.parametrosDAO = new ParametrosDAO(Parametros.class);
+        this.usuarioDAO = new UsuarioDAO(Usuario.class);
+        this.lista = FXCollections.observableList(parametrosDAO.listAll());
+        this.parametro = new Parametros();
+        this.usuario = new Usuario();
+        this.parametro.setIdParametro(0l);
+        this.usuario.setId(0l);
+    }
+
+    public Parametros getParametro() {
+        return parametro;
+    }
+
+    public void setParametro(Parametros parametro) {
+        this.parametro = parametro;
+    }
+
+    public void setParametro(long idParametro, String cnpj, String inscrEst, String nomefantasia, String razaoSocial, String endereco, String complemento, int numero, String cep, String bairro, String email, String telefone, Cidade cidade, byte[] foto) {
+        this.parametro = new Parametros(idParametro, cnpj, inscrEst, nomefantasia, razaoSocial, endereco, complemento, numero, cep, bairro, email, telefone, cidade, foto);
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    public void setUsuario(Long id, String login, String senha, UNivel nivel, Boolean status) {
+        this.usuario = new Usuario(id, login, senha, nivel, status);
+    }
+
+    @Override
+    public void insert() {
+        parametrosDAO.save(parametro);
+    }
+
+    @Override
+    public void update() {
+        parametrosDAO.update(parametro);
+    }
+
+    @Override
+    public void delete() {
+        if (parametro.getIdParametro() != 0l) {
+            parametrosDAO.delete(parametro);
+        }
+    }
+
+    @Override
+    public void flushObject() {
+        this.parametro = new Parametros();
+        this.parametro.setIdParametro(0l);
+    }
+
+    @Override
+    public ObservableList<Parametros> getLista() {
+        return lista;
+    }
+
+    @Override
+    public void setLista(ObservableList<Parametros> lista) {
+        this.lista = lista;
+    }
+
+    @Override
+    public void setItenLista(Parametros obj) {
+        this.lista.add(obj);
+    }
+}
