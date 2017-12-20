48a6294945c1b91c880a1329809d188ac155085a
joao <joao@joaoDesk>
2017-12-20 19:11:28 -0200

    Movidos controller para pacote controller. adicionada tab a gerenciador de tabs

--- src/main/java/com/cashf/controller/prepreparo/TabPrePreparoFXMLController.java
+++ src/main/java/com/cashf/controller/prepreparo/TabPrePreparoFXMLController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fxml.prePreparo;
+package com.cashf.controller.prepreparo;
 
 import com.cashf.model.produto.Produto;
 import com.cashf.model.produto.UnidadeMedida;
