795875f1bfb97e93cebfb2725b6f1aa947dfe6f4
joao <joao@joaoDesk>
2017-12-20 19:02:06 -0200

    Gerado controller de interface PRePreparo

---
+++ src/main/resources/fxml/prePreparo/TabPrePreparoFXMLController.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fxml.prePreparo;
+
+import com.cashf.model.produto.Produto;
+import com.cashf.model.produto.UnidadeMedida;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import controller.GenericViewController;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class
+ *
+ * @author joao
+ */
+public class TabPrePreparoFXMLController implements GenericViewController, Initializable {
+
+    @FXML
+    private JFXComboBox<Produto> ccbItens;
+    @FXML
+    private JFXComboBox<UnidadeMedida> cbbUnidadeMedida;
+    @FXML
+    private JFXTextField txtqtde;
+    @FXML
+    private JFXButton btnAdicionar;
+    @FXML
+    private JFXRadioButton rbtCodigo;
+    @FXML
+    private JFXRadioButton rbtDescricao;
+    @FXML
+    private TableView<Produto> tbvItens;
+    @FXML
+    private TableColumn<Produto, String> tbcItem;
+    @FXML
+    private TableColumn<Produto, Integer> tbcQtdIten;
+    @FXML
+    private TableColumn<Produto, String> tbcUnidadeItem;
+    @FXML
+    private TableColumn<Produto, BigDecimal> tbcCustoItem;
+    @FXML
+    private TableColumn btnExcluir;
+    @FXML
+    private JFXButton btnSalvar;
+    @FXML
+    private JFXButton btnNovo;
+    @FXML
+    private JFXButton btnLimpar;
+    @FXML
+    private TableView<Produto> tbvReceita;
+    @FXML
+    private TableColumn<Produto, String> tbcProduto;
+    @FXML
+    private TableColumn<Produto, Integer> tbcQtde;
+    @FXML
+    private TableColumn<Produto, String> tbcUnidade;
+    @FXML
+    private JFXComboBox<Produto> cbbProduto;
+    @FXML
+    private JFXTextField txtRendimento;
+    @FXML
+    private JFXRadioButton rbtCod;
+    @FXML
+    private JFXRadioButton rbtDesc;
+    @FXML
+    private Label lblCustoTotal;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onAdicionar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onSalvar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onNovo(ActionEvent event) {
+    }
+
+    @FXML
+    private void onExcluir(ActionEvent event) {
+    }
+
+    @FXML
+    private void onLimpar(ActionEvent event) {
+    }
+
+    @Override
+    public void clearFields() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setInputOff() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setInputOn() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Boolean validateFields() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void loadDataToScreen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
