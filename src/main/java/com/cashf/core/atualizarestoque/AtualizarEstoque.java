aa6a575b2fdc4245c5a6b0ec4ec266478c58c51a
joao <joao@joaoDesk>
2017-12-23 19:29:12 -0200

    Implementação clsse ajuste estoque

--- src/main/java/com/cashf/core/atualizarestoque/AtualizarEstoque.java
+++ src/main/java/com/cashf/core/atualizarestoque/AtualizarEstoque.java
@@ -9,7 +9,10 @@ import com.cashf.dao.ajusteestoque.AjusteEstoqueDAO;
 import com.cashf.dao.produto.ProdutoDAO;
 import com.cashf.model.ajusteestoque.AjusteEstoque;
 import com.cashf.model.ajusteestoque.TipoAjuste;
+import com.cashf.model.prepreparo.PrePreparo;
 import com.cashf.model.produto.Produto;
+import com.cashf.model.produto.UnidadeMedida;
+import java.math.BigDecimal;
 
 /**
  *
@@ -20,6 +23,7 @@ public class AtualizarEstoque {
     private final AjusteEstoqueDAO ajusteEstoqueDAO;
     private final ProdutoDAO produtoDAO;
     private Produto produto;
+    private PrePreparo prePreparo;
     private AjusteEstoque ajusteEstoque;
     private TipoAjuste tipoAjuste;
 
@@ -28,6 +32,29 @@ public class AtualizarEstoque {
         this.ajusteEstoqueDAO = new AjusteEstoqueDAO(AjusteEstoque.class);
         this.produto = new Produto();
         produto.setIdProduto(0l);
+        this.prePreparo = new PrePreparo();
+        prePreparo.setIdPrepreparo(0l);
+    }
+
+    public Boolean ajustarEstoqueProduto(Produto produto, BigDecimal qtdeAjuste, UnidadeMedida unidadeMedida) {
+        boolean flag = true;
+        BigDecimal qtdeAtual = produto.getQtdeProduto();
+        if (produto.getUnidadeMedida().equals(unidadeMedida)) {
+            if (qtdeAtual.compareTo(qtdeAjuste) >= 0) {//Se unidades iguáis e qtde>= a ajuste
+                produto.setQtdeProduto(produto.getQtdeProduto().subtract(qtdeAjuste));
+            }
+        } else {
+
+        }
+        return flag;
+    }
+
+    public PrePreparo getPrePreparo() {
+        return prePreparo;
+    }
+
+    public void setPrePreparo(PrePreparo prePreparo) {
+        this.prePreparo = prePreparo;
     }
 
     public Produto getProduto() {
