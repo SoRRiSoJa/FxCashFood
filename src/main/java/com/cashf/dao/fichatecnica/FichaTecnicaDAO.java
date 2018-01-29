50b59041f9ea6a51b17cf5c41066a9b3ca708365
joao <joao@joaoDesk>
2018-01-29 16:16:15 -0200

    Fichatecnica DAO

---
+++ src/main/java/com/cashf/dao/fichatecnica/FichaTecnicaDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cashf.dao.fichatecnica;
+
+import com.cashf.model.fichatecnica.FichaTecnica;
+import dao.GenericDAOIMP;
+
+/**
+ *
+ * @author joao
+ */
+public class FichaTecnicaDAO extends GenericDAOIMP<FichaTecnica>{
+    public FichaTecnicaDAO(Class<FichaTecnica> clazz) {
+        super(clazz);
+    }
+    
+}
