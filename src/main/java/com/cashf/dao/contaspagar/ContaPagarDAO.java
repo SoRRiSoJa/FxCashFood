ef1226aa0dd76f903027856053641bdb6f4f3461
joao <joao@joaoDesk>
2018-02-20 16:45:23 -0300

    Implementado DAO para ContaPagar

---
+++ src/main/java/com/cashf/dao/contaspagar/ContaPagarDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cashf.dao.contaspagar;
+
+import com.cashf.model.contasPagar.ContaPagar;
+import dao.GenericDAOIMP;
+
+/**
+ *
+ * @author joao
+ */
+public class ContaPagarDAO extends GenericDAOIMP<ContaPagar> {
+
+    public ContaPagarDAO(Class<ContaPagar> clazz) {
+        super(clazz);
+    }
+
+}
