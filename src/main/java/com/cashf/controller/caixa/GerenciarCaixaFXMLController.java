14c9307cfbf56421cd6995eca8d724287671bd5f
joao <joao@joaoDesk>
2018-03-14 01:12:58 -0300

    Testado abrir Caixa, funcionado

--- src/main/java/com/cashf/controller/caixa/GerenciarCaixaFXMLController.java
+++ src/main/java/com/cashf/controller/caixa/GerenciarCaixaFXMLController.java
@@ -6,22 +6,42 @@
 package com.cashf.controller.caixa;
 
 import com.cashf.cashfood.MainApp;
+import com.cashf.model.caixa.Caixa;
+import com.cashf.model.caixa.CaixaMovimento;
+import com.cashf.model.caixa.TPMov;
 import com.jfoenix.controls.JFXButton;
+import com.sun.prism.impl.Disposer;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import javafx.util.Duration;
+import org.apache.derby.client.am.DateTime;
+import org.controlsfx.control.Notifications;
 import util.PoupUpUtil;
 
 /**
@@ -32,15 +52,17 @@ import util.PoupUpUtil;
 public class GerenciarCaixaFXMLController implements Initializable {
 
     @FXML
-    private TableView<?> tbvMovimentacoes;
+    private TableView<CaixaMovimento> tbvMovimentacoes;
     @FXML
-    private TableColumn<?, ?> tbcDescricao;
+    private TableColumn<CaixaMovimento, String> tbcDescricao;
     @FXML
-    private TableColumn<?, ?> tbcTipo;
+    private TableColumn<CaixaMovimento, TPMov> tbcTipo;
     @FXML
-    private TableColumn<?, ?> tbcValor;
+    private TableColumn<CaixaMovimento, BigDecimal> tbcValor;
     @FXML
-    private TableColumn<?, ?> tbcSaldo;
+    private TableColumn<CaixaMovimento, DateTime> tbcData;
+    @FXML
+    private TableColumn btnEstornar;
     @FXML
     private JFXButton btnAbrirCaixa;
     @FXML
@@ -56,13 +78,14 @@ public class GerenciarCaixaFXMLController implements Initializable {
     @FXML
     private Label lblSaldo;
     @FXML
-    private TableView<?> tbvLista;
+    private TableView<Caixa> tbvLista;
     @FXML
-    private TableColumn<?, ?> tbcDataAb;
+    private TableColumn<Caixa, LocalDate> tbcDataAb;
     @FXML
-    private TableColumn<?, ?> tbcDataFe;
+    private TableColumn<Caixa, LocalDate> tbcDataFe;
     @FXML
     private Pane paneRoot;
+   
 
     /**
      * Initializes the controller class.
@@ -70,6 +93,7 @@ public class GerenciarCaixaFXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        setUpTbv();
     }
 
     @FXML
@@ -109,5 +133,81 @@ public class GerenciarCaixaFXMLController implements Initializable {
         } catch (IOException ex) {
             System.out.println("Erro---->" + ex);
         }
+    }
+
+    private void setUpTbv() {
+        tbcDescricao.setCellValueFactory(new PropertyValueFactory<>("observacao"));
+        tbcData.setCellValueFactory(new PropertyValueFactory<>("data"));
+        tbcTipo.setCellValueFactory(new PropertyValueFactory<>("tipoMovimento"));
+        tbcValor.setCellValueFactory(new PropertyValueFactory<>("valor"));
+        btnEstornar.setCellFactory(
+                new Callback<TableColumn<Disposer.Record, Boolean>, TableCell<Disposer.Record, Boolean>>() {
+            @Override
+            public TableCell<Disposer.Record, Boolean> call(TableColumn<Disposer.Record, Boolean> p) {
+                return new ButtonCellCashBack();
+            }
+        });
+        tbvMovimentacoes.getColumns().setAll(tbcDescricao, tbcTipo, tbcValor, btnEstornar);
+    }
+
+    public class ButtonCellCashBack extends TableCell<Disposer.Record, Boolean> {
+
+        Image img;
+        ImageView imgv;
+        JFXButton cellButton = new JFXButton("Estornar");
+        Notifications notificationBuilder;
+
+        public ButtonCellCashBack() {
+            this.img = new Image("Imagens/cash_back2-256.png");
+            this.imgv = new ImageView(img);
+            cellButton.setGraphic(imgv);
+            cellButton.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            cellButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent t) {
+                    // get Selected Item
+                    CaixaMovimento currentPerson = (CaixaMovimento) ButtonCellCashBack.this.getTableView().getItems().get(ButtonCellCashBack.this.getIndex());
+                    //remove selected item from the table list
+                    if (currentPerson != null) {
+
+                        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                        alert.setTitle("Cofirmar Estornar Lançamento!");
+                        alert.setHeaderText("Deseja realmente Estornar?");
+                        alert.setContentText("Movimento:(" + currentPerson.getData() + "|" + currentPerson.getObservacao());
+                        Optional<ButtonType> result = alert.showAndWait();
+                        if (result.get() == ButtonType.OK) {
+                            // ... user chose OK
+                            CaixaController.getInstance().getListaMov().remove(currentPerson);
+                            notificationBuilder = Notifications.create().title("MOvimento Estornado!").
+                                    text("Estorno realizado com sucesso.").
+                                    hideAfter(Duration.seconds(2)).
+                                    position(Pos.TOP_RIGHT).
+                                    darkStyle();
+                            notificationBuilder.showInformation();
+                        } else {
+                            alert.close();
+                        }
+                    } else {
+                        notificationBuilder = Notifications.create().title("Nenhum item selecionado!").
+                                text("Você deve selecionar uma movimentação da lista para estornar.").
+                                hideAfter(Duration.seconds(2)).
+                                position(Pos.TOP_RIGHT).
+                                darkStyle();
+                        notificationBuilder.showConfirm();
+                    }
+                }
+            });
+        }
+
+        @Override
+        protected void updateItem(Boolean t, boolean empty) {
+            super.updateItem(t, empty);
+            if (!empty) {
+                setGraphic(cellButton);
+            } else {
+                setGraphic(null);
+            }
+        }
+
     }
 }
