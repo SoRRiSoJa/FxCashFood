00f934b92824e8af78e036c83749d0c4f1347a14
joao <joao@joaoDesk>
2018-09-20 16:58:07 -0300

    Query relatorio ranking de produtos em desenvolvimento

--- src/main/java/com/cashf/dao/venda/VendaDAO.java
+++ src/main/java/com/cashf/dao/venda/VendaDAO.java
@@ -6,8 +6,11 @@
 package com.cashf.dao.venda;
 
 import com.cashf.model.cliente.Cliente;
+import com.cashf.model.venda.ProdutoVenda;
 import com.cashf.model.venda.Venda;
 import dao.GenericDAOIMP;
+import java.util.List;
+import java.util.Map;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
@@ -42,6 +45,17 @@ public class VendaDAO extends GenericDAOIMP<Venda> {
         } catch (Exception e) {
             System.out.println("Erro:" + e);
             return 0;
+        }
+
+    }
+    public List<Map> rankingProdutos() {
+        try (Session session = sessionFactory.openSession()) {
+            String hql = "select new map(distinct pdv.produto.descriao as produto,sum(pdv.produto.qtde) as qtde,precoUnit as preco) from ProdutoVenda pdv";
+            Query query = session.createQuery(hql);
+            return query.list();
+        } catch (Exception e) {
+            System.out.println("Erro:" + e);
+            return null;
         }
 
     }
