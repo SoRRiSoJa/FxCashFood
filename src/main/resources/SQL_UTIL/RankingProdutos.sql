fc39a71a2bea84ab3d49793c552348d1f1ce0f0a
joao <joao@joaoDesk>
2018-09-21 16:22:15 -0300

    Query SQL para Ranking /HQL em desenvolvimento

---
+++ src/main/resources/SQL_UTIL/RankingProdutos.sql
@@ -0,0 +1,9 @@
+﻿SELECT  distinct PV.idproduto,
+PD.descriao,
+PV.qtde,
+PV.precounit,
+PV.qtde*PV.precounit as total_venda,
+PV.qtde*PD.preco_custo as custo_total,
+(PV.qtde*PV.precounit)/(PV.qtde*PD.preco_custo) as margem
+  FROM public.produto_venda PV inner join public.produto PD ON PV.idproduto=PD.idproduto;
+
